9113812c4ecad5f2f3c599ead75328a0fea46a0c
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-21 11:49:30 -0700

    Add cypress before each and writing tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Main Reservations Page', () => {
+  beforeEach(() => {
+    cy.request('http://localhost:3001/api/v1/reservations')
+    // cy.intercept('GET', 'http://localhost:3000/', {fixture: '../fixtures/mainPage.json'})
+  })
+  it('should display a logo on main page',() => {
+    cy.get('.app-title').contains("Turing Cafe Reservations")
+  })
+  it('should have a container that holds all of the reservations', () =>{
+    cy.get('.dashboard-container').children().should("have.length", "2")
+  })
+  it('should display a form on the page on load with four inputs and a button', () => {
+    cy.get('form-container').children().should("have.length", "5")
   })
 })
