096eb3a034d964ce5b08e28a91684e0c0bbe8f19
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-21 09:37:50 -0700

    Build in card skeleton no props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,1 +1,16 @@
 import './Card.css'
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='reservation-container'>
+      <h2 className='name'></h2>
+      <p className='info'></p>
+      <p className='info'></p>
+      <p className='info'></p>
+      <button className='cancel-btn'>Cancel</button>
+    </div>
+  )
+}
+
+export default Card
