edbf0e75f6d3afcd3959b34f04435de63700bfac
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-21 09:46:14 -0700

    Add API functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import getAPIData from '../../APICalls';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount = () => {
+    getAPIData()
+    .then((data)=> this.setState({ reservations: data}))
+    .catch((error)=> console.log(error))
+  }
+
+  componentDidUpdate = () => {
+    console.log(this.state.reservations)
+  }
+  
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <div className='resy-form'></div>
+        <div className='resy-container'></div>
       </div>
     )
   }
